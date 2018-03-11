539dea63c1fbbcde627042f87d694348dce63d76
Eivind Samuelsen <eivind.samuelsen@gmail.com>
2018-03-11 21:23:46 +0100

    Made Space allocate first pool block off locally, added basic algebra / linear operations

--- include/dvs.h
+++ include/dvs.h
@@ -7,10 +7,156 @@
 namespace dvs
 {
 	template <class S>
+	class Space;
+	template <class S>
+	class Vector
+	{
+		S* _values;
+		Space<S>& _space;
+	public:
+		Vector(S* values, Space<S>& space) noexcept : _values(values), _space(space) { }
+		~Vector() noexcept
+		{
+			if (_values)
+				_space._dealloc(_values);
+		}
+		Vector() = delete;
+		Vector(Vector&& b) noexcept : _values(b._values), _space(b._space)
+		{
+			b._values = nullptr;
+		}
+		Vector(const Vector& b) noexcept : _values(b._space._alloc()), _space(b._space())
+		{
+			for (int i = 0; i < _space._dim; ++i)
+				_values[i] = b[i];
+		}
+
+		int spaceCheck(const Vector& b) const noexcept
+		{
+			assert(&_space == &b._space);
+			return _space._dim;
+		}
+
+		Vector& operator=(Vector&& b) noexcept
+		{
+			assert(&_space == &b._space);
+			if (&b == this)
+				return *this;
+			if (_values)
+				_space._dealloc(_values);
+			_values = b._values;
+			b._values = nullptr;
+			return *this;
+		}
+		Vector& operator=(const Vector& b) noexcept
+		{
+			if (!_values)
+				_values = _space._alloc();
+			for (int i = 0, c = spaceCheck(b); i < c; ++i)
+				_values[i] = b[i];
+			return *this;
+		}
+
+		Space<S>& space() const { return _space; }
+		int size() const { return _space._dim; }
+
+		S& operator[](int i) { return _values[i]; }
+		const S& operator[](int i) const { return _values[i]; }
+
+		S* begin() { return _values; }
+		const S* begin() const { return _values; }
+		S* end() { return _values + _space._dim; }
+		const S* end() const { return _values + _space._dim; }
+
+		Vector& operator+=(const Vector& b)
+		{
+			for (int i = 0, c = spaceCheck(c); i < c; ++i)
+				_values[i] += b[i];
+			return *this;
+		}
+		Vector& operator+=(S b)
+		{
+			for (int i = 0, c = size(); i < c; ++i)
+				_values[i] += b;
+			return *this;
+		}
+		Vector& operator-=(const Vector& b)
+		{
+			for (int i = 0, c = spaceCheck(b); i < c; ++i)
+				_values[i] -= b[i];
+			return *this;
+		}
+		Vector& operator-=(S b)
+		{
+			for (int i = 0, c = size(); i < c; ++i)
+				_values[i] -= b;
+			return *this;
+		}
+		Vector& operator*=(const Vector& b)
+		{
+			for (int i = 0, c = spaceCheck(b); i < c; ++i)
+				_values[i] *= b[i];
+			return *this;
+		}
+		Vector& operator*=(S b)
+		{
+			for (int i = 0, c = size(); i < c; ++i)
+				_values[i] *= b;
+			return *this;
+		}
+		Vector& operator/=(const Vector& b)
+		{
+			for (int i = 0, c = spaceCheck(b); i < c; ++i)
+				_values[i] /= b[i];
+			return *this;
+		}
+		Vector& operator/=(S b)
+		{
+			for (int i = 0, c = size(); i < c; ++i)
+				_values[i] /= b;
+			return *this;
+		}
+
+		Vector& negate(const Vector& b)
+		{
+			for (int i = 0, c = spaceCheck(b); i < c; ++i)
+				_values[i] = b[i] - _values[i];
+			return *this;
+		}
+		Vector& negate(S b)
+		{
+			for (int i = 0, c = size(); i < c; ++i)
+				_values[i] = b - _values[i];
+			return *this;
+		}
+		Vector& negate()
+		{
+			for (int i = 0, c = size(); i < c; ++i)
+				_values[i] = -_values[i];
+			return *this;
+		}
+
+		Vector& invert(const Vector& b)
+		{
+			for (int i = 0, c = spaceCheck(b); i < c; ++i)
+				_values[i] = b[i] / _values[i];
+			return *this;
+		}
+		Vector& invert(S b)
+		{
+			for (int i = 0, c = size(); i < c; ++i)
+				_values[i] = b / _values[i];
+			return *this;
+		}
+	};
+	template <class S>
 	class Space
 	{
 		static_assert(std::is_trivially_default_constructible_v<S>);
 		static_assert(std::is_trivially_destructible_v<S>);
+		friend class Vector<S>;
+
+		using Vector = Vector<S>;
 	public:
 		struct Free
 		{
@@ -25,62 +171,6 @@ namespace dvs
 			Block(Block* prev) : prev(prev) { }
 		};
 		static_assert(sizeof(Block) <= 4096);
-
-		class Vector
-		{
-			S* _values;
-			Space* const _space;
-		public:
-			Vector(S* values, Space* space) noexcept : _values(values), _space(space)
-			{
-				assert(_space != nullptr);
-			}
-			~Vector() noexcept
-			{
-				if (_values)
-					_space->_dealloc(_values);
-			}
-			Vector() = delete;
-			Vector(Vector&& b) noexcept : _values(b._values), _space(b._space)
-			{
-				b._values = nullptr;
-			}
-			Vector(const Vector& b) noexcept : _values(b._space->_alloc()), _space(b._space)
-			{
-				for (int i = 0; i < _space->_dim; ++i)
-					_values[i] = b._values[i];
-			}
-
-			Vector& operator=(Vector&& b) noexcept
-			{
-				assert(_space == b._space);
-				if (_values)
-					_space->_dealloc(_values);
-				_values = b._values;
-				b._values = nullptr;
-				return *this;
-			}
-			Vector& operator=(const Vector& b) noexcept
-			{
-				assert(_space == b._space);
-				if (!_values)
-					_values = _space->_alloc();
-				for (int i = 0; i < _space->_dim; ++i)
-					_values[i] = b._values[i];
-				return *this;
-			}
-
-			Space& space() const { return *_space; }
-			int size() const { return _space->_dim; }
-
-			      S& operator[](int i)       { return _values[i]; }
-			const S& operator[](int i) const { return _values[i]; }
-
-			      S* begin()       { return _values; }
-			const S* begin() const { return _values; }
-			      S* end()       { return _values + _space->_dim; }
-			const S* end() const { return _values + _space->_dim; }
-		};
 
 		Block _root_block;
 		const int _dim;
@@ -156,12 +246,12 @@ namespace dvs
 		{
 			assert(sizeof(Free) <= sizeof(S)*_dim);
 			_zero_basis = reinterpret_cast<Vector*>(new char[sizeof(Vector)*(_dim + 1)]);
-			new (_zero_basis) Vector(_alloc(), this);
+			new (_zero_basis) Vector(_alloc(), *this);
 			for (int i = 0; i < _dim; ++i)
 				_zero_basis[0][i] = S(0);
 			for (int k = 0; k < _dim; ++k)
 			{
-				new (_zero_basis + k + 1) Vector(_alloc(), this);
+				new (_zero_basis + k + 1) Vector(_alloc(), *this);
 				for (int i = 0; i < _dim; ++i)
 					_zero_basis[k+1][i] = i == k ? S(1) : S(0);
 			}
@@ -183,18 +273,99 @@ namespace dvs
 		const Vector& zero() const { return _zero_basis[0]; }
 		const Vector& basis(int i) const { return _zero_basis[i + 1]; }
 
-		template <class... Components>
+		template <class... Components, class Enable = std::enable_if_t<sizeof...(Components) >= 2>>
 		Vector operator()(Components&&... c)
 		{
 			assert(sizeof...(Components) == _dim);
-			Vector result(_alloc(), this);
+			Vector result(_alloc(), *this);
 			int i = 0;
 			((result[i++] = S(std::forward<Components>(c))), ...);
 			return result;
+		}
+		template <class T, class Enable = ifExpr<T>>
+		Vector operator()(const T& e)
+		{
+			assert(this == &e.space());
+			return { e };
 		}
 
 		int dim() const { return _dim; }
 	};
 
+	template <class S> Vector<S> operator-(      Vector<S>&& a) { return std::move(a.negate()); }
+	template <class S> Vector<S> operator-(const Vector<S>&& a) { auto r = a; a.negate(); return r; }
 
+	template <class S> Vector<S> operator+(      Vector<S>&& a,       Vector<S>&& b) { return std::move(a += b); }
+	template <class S> Vector<S> operator+(      Vector<S>&& a, const Vector<S>&  b) { return std::move(a += b); }
+	template <class S> Vector<S> operator+(const Vector<S>&  a,       Vector<S>&& b) { return std::move(b += a); }
+	template <class S> Vector<S> operator+(const Vector<S>&  a, const Vector<S>&  b) { auto r = a; r += b; return r; }
+	template <class S> Vector<S> operator+(Vector<S>&& a,      S  b) { return std::move(a += b); }
+	template <class S> Vector<S> operator+(S  a,      Vector<S>&& b) { return std::move(b += a); }
+	template <class S> Vector<S> operator+(const Vector<S>& a, S  b) { auto r = a; r += b; return r; }
+	template <class S> Vector<S> operator+(S  a, const Vector<S>& b) { auto r = b; r += a; return r; }
+
+	template <class S> Vector<S> operator-(      Vector<S>&& a,       Vector<S>&& b) { return std::move(a -= b); }
+	template <class S> Vector<S> operator-(      Vector<S>&& a, const Vector<S>&  b) { return std::move(a -= b); }
+	template <class S> Vector<S> operator-(const Vector<S>&  a,       Vector<S>&& b) { return std::move(b.negate(a)); }
+	template <class S> Vector<S> operator-(const Vector<S>&  a, const Vector<S>&  b) { auto r = a; r -= b; return r; }
+	template <class S> Vector<S> operator-(Vector<S>&& a,      S  b) { return std::move(a -= b); }
+	template <class S> Vector<S> operator-(S  a,      Vector<S>&& b) { return std::move(b.negate(a)); }
+	template <class S> Vector<S> operator-(const Vector<S>& a, S  b) { auto r = a; r -= b; return r; }
+	template <class S> Vector<S> operator-(S a, const Vector<S>&  b) { auto r = b; r.negate(b); return r; }
+
+	template <class S> Vector<S> operator*(      Vector<S>&& a,       Vector<S>&& b) { return std::move(a *= b); }
+	template <class S> Vector<S> operator*(      Vector<S>&& a, const Vector<S>&  b) { return std::move(a *= b); }
+	template <class S> Vector<S> operator*(const Vector<S>&  a,       Vector<S>&& b) { return std::move(b *= a); }
+	template <class S> Vector<S> operator*(const Vector<S>&  a, const Vector<S>&  b) { auto r = a; r *= b; return r; }
+	template <class S> Vector<S> operator*(Vector<S>&& a,      S  b) { return std::move(a *= b); }
+	template <class S> Vector<S> operator*(S  a,      Vector<S>&& b) { return std::move(b *= a); }
+	template <class S> Vector<S> operator*(const Vector<S>& a, S  b) { auto r = a; r *= b; return r; }
+	template <class S> Vector<S> operator*(S  a, const Vector<S>& b) { auto r = b; r *= a; return r; }
+
+	template <class S> Vector<S> operator/(      Vector<S>&& a,       Vector<S>&& b) { return std::move(a /= b); }
+	template <class S> Vector<S> operator/(      Vector<S>&& a, const Vector<S>&  b) { return std::move(a /= b); }
+	template <class S> Vector<S> operator/(const Vector<S>&  a,       Vector<S>&& b) { return std::move(b.invert(a)); }
+	template <class S> Vector<S> operator/(const Vector<S>&  a, const Vector<S>&  b) { auto r = a; r /= b; return r; }
+	template <class S> Vector<S> operator/(Vector<S>&& a,      S  b) { return std::move(a /= b); }
+	template <class S> Vector<S> operator/(S  a,      Vector<S>&& b) { return std::move(b.invert(a)); }
+	template <class S> Vector<S> operator/(const Vector<S>& a, S  b) { auto r = a; r /= b; return r; }
+	template <class S> Vector<S> operator/(S a, const Vector<S>&  b) { auto r = b; r.invert(b); return r; }
+
+	template <class S>
+	auto dot(const Vector<S>& a, const Vector<S>& b) 
+	{
+		auto sp = a[0] * b[0];
+		for (int i = 1, c = a.spaceCheck(b); i < c; ++i)
+			sp += a[i] * b[i];
+		return sp;
+	}
+
+	template <class S> 
+	auto square(const Vector<S>& a)
+	{
+		auto ss = a[0] * a[0];
+		for (int i = 1, c = a.size(); i < c; ++i)
+			ss += a[i] * a[i];
+		return ss;
+	}
+
+	template <class S>
+	S length(const Vector<S>& a)
+	{
+		return sqrt(square(a));
+	}
+	
+	template <class S>
+	struct Decomposition
+	{
+		S length;
+		Vector<S> direction;
+
+		Decomposition(Vector<S> a) : length(dvs::length(a)), direction(std::move(a)/length) { }
+	};
+	template <class S> Decomposition<S> decompose(      Vector<S>&& a) { return { std::move(a) }; }
+	template <class S> Decomposition<S> decompose(const Vector<S>&  a) { return {           a  }; }
+
+	template <class S> Vector<S> direction(      Vector<S>&& a) { return decompose(std::move(a)).direction; }
+	template <class S> Vector<S> direction(const Vector<S>&  a) { return decompose(          a ).direction; }
 }
--- test/test.cpp
+++ test/test.cpp
@@ -7,6 +7,7 @@ int main()
 	const auto foo = R3(1, 2, 3);
 	const auto bar = foo.space()(4, 5, 6);
 
+	const auto baz = R3(0, 1, 2) - R3(2, 1, 0);
 
 	return 0;
 }
